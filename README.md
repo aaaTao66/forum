a22657622dd7ac60dd1767e08472d33c8f9c8033
YuXiantao <17663083790@163.com>
2019-08-29 15:47:37 +0800

    update 2019/8/29

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ## 类比
 可以参考: https://bbs.deepin.org/ 这个网站的论坛,完成后比较像他们这种。
 ## 个人
-个人博客地址: https://aaatao66.github.io
+个人博客地址: https://aaatao66.github.io <br>
 以及掘金地址: https://juejin.im/user/5d187331f265da1bc5527953
 ## 感谢
 我都是根据码匠笔记的教程进行开发的, 大家可以关注 -微信公众号: 码匠笔记
---
+++ src/main/java/life/aaatao/forum/provider/GithubProvider.java
@@ -0,0 +1,56 @@
+package life.aaatao.forum.provider;
+
+
+import com.alibaba.fastjson.JSON;
+import life.aaatao.forum.dto.AccessTokenDTO;
+import life.aaatao.forum.dto.GithubUser;
+import okhttp3.*;
+import okhttp3.Request.Builder;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class GithubProvider {
+
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            String token = string.split("&")[0].split("=")[1];
+            return token;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public GithubUser getUser(String accessTocken){
+        OkHttpClient client = new OkHttpClient();
+        String url;
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token=" + accessTocken)
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            //自动转换成 java类 对象
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+        }
+        return null;
+    }
+
+}
