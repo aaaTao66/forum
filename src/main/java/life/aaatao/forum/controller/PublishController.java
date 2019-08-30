2aee599d69fc54d836a8f1cf1e99be6151b086a4
YuXiantao <17663083790@163.com>
2019-08-30 17:32:06 +0800

    add publish.html & PublishController

---
+++ src/main/java/life/aaatao/forum/controller/PublishController.java
@@ -0,0 +1,13 @@
+package life.aaatao.forum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PublishController {
+
+    @GetMapping("/publish")
+    public String publish() {
+        return "publish";
+    }
+}
