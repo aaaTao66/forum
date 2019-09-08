1476dfc19e89dab065a75d7c024af3c49523db64
Carson <aaatao66@gmail.com>
2019-09-08 18:44:42 +0800

    add 我的问题页面(一) & 热部署

---
+++ src/main/java/life/aaatao/forum/controller/ProfileController.java
@@ -0,0 +1,24 @@
+package life.aaatao.forum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ProfileController {
+
+
+    @GetMapping("/profile/{action}")
+    public String profile(@PathVariable(name = "action") String action,
+                          Model model) {
+        if ("questions".equals(action)) {
+            model.addAttribute("section", "questions");
+            model.addAttribute("sectionName", "我的提问");
+        } else if ("replies".equals(action)) {
+            model.addAttribute("section", "replies");
+            model.addAttribute("sectionName", "最新回复");
+        }
+        return "profile";
+    }
+}
