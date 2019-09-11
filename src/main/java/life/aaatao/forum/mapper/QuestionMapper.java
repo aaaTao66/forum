709b956f957f3bb3ca8b05baa7bf0abc21e8fdd5
Carson <aaatao66@gmail.com>
2019-09-11 16:18:53 +0800

    完成问题详情页面

--- src/main/java/life/aaatao/forum/mapper/QuestionMapper.java
+++ src/main/java/life/aaatao/forum/mapper/QuestionMapper.java
@@ -2,6 +2,7 @@ package life.aaatao.forum.mapper;
 
 
 import life.aaatao.forum.domain.Question;
+import life.aaatao.forum.dto.QuestionDTO;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -27,4 +28,7 @@ public interface QuestionMapper {
 
     @Select("select count(1) from question where creator = #{userId}")
     Integer countByUserId(@Param("userId") Integer userId);
+
+    @Select("select * from question where id = #{id}")
+    Question getById(@Param("id") Integer id);
 }
